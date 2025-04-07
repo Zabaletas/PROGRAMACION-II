2cc7e52247df5a409ed94997faf258f8b7ae646d
Zabaletas <sergigati@gmail.com>
2025-04-07 05:49:05 -0400

    Update App.java

--- Practica 7/jueegoPOO/src/jueegoPOO/App.java
+++ Practica 7/jueegoPOO/src/jueegoPOO/App.java
@@ -4,13 +4,10 @@ public class App {
         JuegoAdivinaNumero juegoNormal = new JuegoAdivinaNumero(3);
         JuegoAdivinaPar juegoPar = new JuegoAdivinaPar(3);
         JuegoAdivinaImpar juegoImpar = new JuegoAdivinaImpar(3);
-
         System.out.println("----- JUEGO NORMAL -----");
         juegoNormal.juega();
-
         System.out.println("\n----- JUEGO PAR -----");
         juegoPar.juega();
-
         System.out.println("\n----- JUEGO IMPAR -----");
         juegoImpar.juega();
     }
